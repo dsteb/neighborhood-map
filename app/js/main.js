3545f8ed41fb637562b215d69f7e70bb764b81d1
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-10-05 00:23:14 +0200

    feat: Add markers with empty infowindows

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,15 @@ var app = app || {};
 
   'use strict';
 
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, i) {
+      return typeof args[i] !== 'undefined' ? args[i] : match;
+    });
+  };
+
+  app.key = 'AIzaSyAtkSd9kZmhNgLPd6cju7AnqU7MTr4u6ZE';
+
   function Place(place) {
     this.name = place.name;
   }
@@ -24,7 +33,7 @@ var app = app || {};
 
   function handleError(response, msg) {
     console.error(response, msg);
-    alert('Error')
+    alert('Error');
   }
 
   function AppViewModel(map) {
@@ -33,12 +42,41 @@ var app = app || {};
     self.places = ko.observableArray();
 
     $.get('js/model.json')
-      .done(processModel)
-      .fail(handleError);
+      .done(function(data) {processModel(self, data, map);})
+      .fail(handleError);      
+  }
 
-    function processModel(data) {
-      var places = data.map(function(p) {return new Place(p)});
-      places.forEach(function(p) {self.places.push(p)});
+  function processModel(viewModel, data, map) {
+    var places = data.map(function(p) {return new Place(p);});
+    places.forEach(function(place) {
+      viewModel.places.push(place);
+      getGeoData(map, place);
+    });
+  }
+
+  function getGeoData(map, place) {
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json?address={0}, {1}&key=' + app.key;
+    url = url.format(place.name, Place.city);
+    $.getJSON(url)
+      .done(function(data) { createMarker(map, place, data.results[0]);})
+      .fail(handleError);
+  }
+
+  function createMarker(map, place, data) {
+    var marker = new google.maps.Marker({
+      title: data.formatted_address,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      position: data.geometry.location,
+    });
+    marker.addListener('click', onMarkerSelect);
+    var contentString = '<h1>Heelllo</h1>';
+    var infoWindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+
+    function onMarkerSelect() {
+      infoWindow.open(map, marker);
     }
   }
 
