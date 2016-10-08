5c794e35d7cd67a11e20790f5d08e8a42925780b
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-10-08 17:58:49 +0200

    chore: Switch grunt-serve to grunt-contrib-connect
    I had npm install dependencies issue with grun-serve.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,44 @@
 
 (function() {
-	'use strict';
+  'use strict';
 
-	module.exports = function(grunt) {
+  module.exports = function(grunt) {
 
-	  grunt.initConfig({
-	    jshint: {
-	      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-	      options: {
-	        globals: {
-	          jQuery: true
-	        }
-	      }
-	    },
-	    watch: {
-	      files: ['<%= jshint.files %>'],
-	      tasks: ['jshint']
-	    },
-	    wiredep: {
-	    	task: {
-	    		src: 'app/**/*.html'
-	    	}
-	    },
-	    serve: {
-	    	options: {
-	    		port: 8000,
-	    	}
-	    },
-	  });
+    grunt.initConfig({
+      jshint: {
+        files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+        options: {
+          globals: {
+            jQuery: true
+          }
+        }
+      },
+      watch: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint']
+      },
+      wiredep: {
+        task: {
+          src: 'app/**/*.html'
+        }
+      },
+      connect: {
+        server: {
+          options: {
+            port: 8000,
+            keepalive: true,
+            open: true,
+          }
+        },
+      },
+    });
 
-	  grunt.loadNpmTasks('grunt-contrib-jshint');
-	  grunt.loadNpmTasks('grunt-contrib-watch');
-	  grunt.loadNpmTasks('grunt-wiredep');
-	  grunt.loadNpmTasks('grunt-serve');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-	  grunt.registerTask('default', ['jshint', 'wiredep', 'serve']);
+    grunt.registerTask('default', ['jshint', 'wiredep', 'connect']);
 
-	};
+  };
 })();
