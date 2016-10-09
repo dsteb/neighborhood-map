5e42054ea311adbe0c0547190306a4aebc39e3db
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-10-09 14:12:40 +0200

    chore: Grunt for building production
    This first version just copy bower_dependencies to
    dist folder and manage index.html with the targethtml.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,94 @@
+module.exports = function(grunt) {
 
-(function() {
   'use strict';
 
-  module.exports = function(grunt) {
-
-    grunt.initConfig({
-      jshint: {
-        files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    wiredep: {
+      task: {
+        src: 'app/**/*.html'
+      }
+    },
+    connect: {
+      server: {
         options: {
-          globals: {
-            jQuery: true
-          }
+          port: 8000,
+          keepalive: true,
+          open: true,
         }
       },
-      watch: {
-        files: ['<%= jshint.files %>'],
-        tasks: ['jshint']
-      },
-      wiredep: {
-        task: {
-          src: 'app/**/*.html'
-        }
-      },
-      connect: {
-        server: {
-          options: {
-            port: 8000,
-            keepalive: true,
-            open: true,
+    },
+    copy: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'bower_components/bootstrap/dist/',
+            src: ['**/*.min.css', '**/*.min.js', 'fonts/**/*'],
+            dest: 'dist/',
+          }, {
+            expand: true,
+            cwd: 'bower_components/jquery/dist/',
+            src: 'jquery.min.js',
+            dest: 'dist/js/',
+          }, {
+            expand: true,
+            cwd: 'bower_components/knockout/dist',
+            src: 'knockout.js',
+            dest: 'dist/js/',
+          }, {
+            expand: true,
+            cwd: 'app/',
+            src: ['**/*.css', '**/*.js', '**/*.json'],
+            dest: 'dist/',
           }
-        },
+        ],
       },
-    });
+    },
+    concat: {
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-wiredep');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+      dist: {
+        src: ['app/js/main.js', 'app/js/model.json'],
+        dest: 'dist/yomen.js'
+      }
+    },
+    uglify: {
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/yomen.min.js'
+      }
+    },
+    targethtml: {
+      dist: {
+        files: {
+          'dist/index.html': 'app/index.html',
+        }
+      }
+    },
+  });
 
-    grunt.registerTask('default', ['jshint', 'wiredep', 'connect']);
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-targethtml');
 
-  };
-})();
+
+  grunt.registerTask('default', ['jshint', 'wiredep', 'connect']);
+  grunt.registerTask('prod', ['jshint', 'copy:dist', 'targethtml:dist']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# neighborhood-map
-Milan Neighborhood Map
+# Milan Neighborhood Map
+This project shows touristic places, restaurants and places for sport leisure activities in Milan on the Google Maps.
+It is single-page responsive application build with Knockout.js framework and hosted on GitHub Pages.
+The project uses [the Google Maps API](https://developers.google.com/maps/), [the Google Street View Image API](https://developers.google.com/maps/documentation/streetview/), [the Google GeoCoding API](https://developers.google.com/maps/documentation/geocoding/intro) and [MediaWiki API](https://www.mediawiki.org/wiki/API:Main_page) to asynchroniously request required information and images for different locations.
+
+## Get Started
+### Installation
+To install and run the project locally, it is required to have `npm`, `bower` and `grunt-cli`.
+
+To run locally:
+```
+npm install && bower install && grunt
+```
+It will startup the server on port 8000: http://localhost:8000/.
+
+### Build for production
+
